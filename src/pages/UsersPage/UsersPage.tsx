167d51b14ee718c126c23510b544ca1d09e368ff
Andrei <i.andrei.abramov@gmail.com>
2022-05-21 22:02:07 +0300

    Loader component created

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,16 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUsers, selectUsersFetchStatus } from 'store/selectors/users-selectors';
 import { useAppDispatch } from 'store/store';
 import { requestUsersThunkAction } from 'store/thunk-actions/users-thunk-actions';
 import { FetchStatus } from 'types/api.types';
 
+import { LoaderDelayed } from 'components/LoaderDelayed';
 import { UsersList } from 'components/UsersList';
 
 export const UsersPage: React.FC = () => {
     const dispatch = useAppDispatch();
     const users = useSelector(selectUsers);
     const usersFetchStatus = useSelector(selectUsersFetchStatus);
+    const [isContentReady, setIsContentReady] = useState(false);
+
+    const isUsersDataFetching = useMemo(
+        () => usersFetchStatus === FetchStatus.Fetching,
+        [usersFetchStatus],
+    );
 
     useEffect(() => {
         if (usersFetchStatus === FetchStatus.Initial) {
@@ -18,5 +25,16 @@ export const UsersPage: React.FC = () => {
         }
     }, [dispatch, usersFetchStatus]);
 
-    return <UsersList users={users} />;
+    return (
+        <>
+            {isContentReady ? (
+                <UsersList users={users} />
+            ) : (
+                <LoaderDelayed
+                    dependencies={[isUsersDataFetching]}
+                    handleContentIsReady={setIsContentReady}
+                />
+            )}
+        </>
+    );
 };
